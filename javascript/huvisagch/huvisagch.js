fec7e0234efd734b218739b0bbec9ce9d5e77e2e
Tugarik <115762126+Tugarik@users.noreply.github.com>
2022-11-30 08:08:15 +0800

    Update huvisagch.js

--- javascript/huvisagch/huvisagch.js
+++ javascript/huvisagch/huvisagch.js
@@ -1,4 +1,7 @@
-let a = 85, b = 75, c = 96, d = 69;
+let a = 85, 
+    b = 75, 
+    c = 96, 
+    d = 69;
 let sum = 0;
 
 if(a > 80) {
@@ -14,3 +17,23 @@ if (d > 80) {
    sum = sum + d;
 }
 console.log(sum);
+
+let e = 3,
+  f = 7,
+  g = 2,
+  h = 4;
+let multiply = 0;
+
+if (e < 5) {
+  multiply = multiply + e;
+}
+if (f < 5) {
+  multiply = multiply * f;
+}
+if (g < 5) {
+  multiply = multiply * g;
+}
+if (h < 5) {
+  multiply = multiply * h;
+}
+console.log(multiply);
