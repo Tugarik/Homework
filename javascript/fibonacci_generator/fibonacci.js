6696b064dbd5fc01b66fa34ab7da6bcb7f29f8fd
Tugarik <tugarik@gmail.com>
2022-12-01 19:14:43 +0800

    fibonacci generator

---
+++ javascript/fibonacci_generator/fibonacci.js
@@ -0,0 +1,20 @@
+// 1 1 2 3 5 8 13 21
+
+let inputNum;
+let a = 1,
+    b = 1;
+let c = a + b;
+let strFibo = " " + a + " " + b;
+do {
+  inputNum = prompt("Too oruulna uu");
+}
+while (Number.isInteger(Number(inputNum)) == false);
+
+while (c < Number(inputNum)) {
+    strFibo += " " + c;
+    a = b;
+    b = c;
+    c = a + b;
+}
+
+console.log(strFibo);
